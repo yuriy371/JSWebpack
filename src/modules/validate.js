33ca6a3ed7dabef33743ab8702271b86a187e301
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-29 21:54:43 +0400

    работа с сабытием blur

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,96 +1,147 @@
 const validate = () => {
     let calcItem = document.querySelectorAll(".calc-item")
-    let formApplicat = document.getElementById("form1")
-    let formReuest = document.getElementById("form2")
-    let formModal = document.getElementById("form3")
+    let formApplicat2 = document.querySelectorAll("#form1")
+    let formReuest2 = document.querySelectorAll("#form2")
+    let formModal2 = document.querySelectorAll("#form3")
 
     let validNumber = /[^\d]/g
-    let validTextName = /[^А-Я" "-]/gi
-    let validEmail = /[^A-Z@-_.!~*']/gi
-    let validPhone = /[^\d()-]/
-    
-    let mess = ""
+    let validTextName = /[^А-Я\s-]/gi
+    let validEmail = /[^a-z@\-_.!~*']{1,}/gi
+    let validPhone = /[^\d()\-+]/g
 
-    let validForm = (name, email, phone, mess) => {
-        let isError = false
-        let nameMess = (mess !== "") ? name.value + mess.value : name.value
+    let check = (item) => {
+        item.value = item.value.replace(/\s+/, " ")
+        item.value = item.value.replace(/-+/, "-")
+        item.value = item.value.replace(/(^[\s\-]+)/g, "").replace(/[\s\-]+$/g, "")
+    }
 
-        if (!validTextName.test(nameMess) && nameMess !== "") {
+    let checkName = (name) => {
+        if (!validTextName.test(name.value) && name.value !== "") {
+            name.value = name.value[0].toUpperCase() + name.value.slice(1)
             console.log("кирилица");
         } else {
-            alert("Ведите текст на русском, поле не должно быть пустым");
-            isError = true
-        }
-
-        if (validEmail.test(email.value) && email.value !== "") {
-            console.log("Правельная почта");
-        } else {
-            alert("Ведите почту, поле не должно быть пустым");
-            isError = true
-        }
-
-        if (validPhone.test(phone.value) && phone.value !== "") {
-            console.log("Правельный телефон");
-        } else {
-            alert("Ведите телефон, поле не должно быть пустым");
-            isError = true
-        }
-
-        if (!isError) {
-            alert("Заявка принета");
+            name.value = ""
         }
     }
 
-    calcItem.forEach(item => {
-        item.addEventListener("keyup", () => {
-            if (!validNumber.test(item.value)) {
-                console.log("число");
-            } else {
-                alert("Введите число");
-            }
+    let checkEmail = (email) => {
+        if (!validEmail.test(email.value) && email.value !== "") {
+            console.log("почта");
+        } else {
+            email.value = ""
+        }
+    }
+
+    let checkPhone = (phone) => {
+        if (!validPhone.test(phone.value) && phone.value !== "") {
+            console.log("Правельный телефон");
+        } else {
+            phone.value = ""
+        }
+    }
+
+    formApplicat2.forEach(item => {
+        let btn = item.querySelector(".form-btn")
+        let inputName = item.querySelector(".form-name")
+        let inputEmail = item.querySelector(".form-email")
+        let inputPhone = item.querySelector(".form-phone")
+
+        inputName.addEventListener("blur", () => {
+            console.log(inputName);
+            check(inputName)
+            checkName(inputName)
+        })
+
+        inputEmail.addEventListener("blur", () => {
+            check(inputEmail)
+            checkEmail(inputEmail)
+        })
+
+        inputPhone.addEventListener("blur", () => {
+            check(inputPhone)
+            checkPhone(inputPhone)
+        })
+
+        btn.addEventListener("click", (e) => {
+            e.preventDefault()
+            inputName.value = ""
+            inputEmail.value = ""
+            inputPhone.value = ""
         })
     })
 
-    formApplicat.addEventListener("submit", (e) => {
-        e.preventDefault()
+    formReuest2.forEach(item => {
+        let btn = item.querySelector(".form-btn")
+        let inputName = item.querySelector(".form-name")
+        let inputEmail = item.querySelector(".form-email")
+        let inputPhone = item.querySelector(".form-phone")
+        let inputMess = item.querySelector(".mess")
 
-        let itemName = formApplicat.querySelector(".form-name")
-        let itemEmail = formApplicat.querySelector(".form-email")
-        let itemPhome = formApplicat.querySelector(".form-phone")
+        inputName.addEventListener("blur", () => {
+            check(inputName)
+            checkName(inputName)
+        })
 
-        validForm(itemName, itemEmail, itemPhome, mess)
-        itemName.value = ""
-        itemEmail.value = ""
-        itemPhome.value = ""
+        inputMess.addEventListener("blur", () => {
+            check(inputMess)
+            checkName(inputMess)
+        })
+
+        inputEmail.addEventListener("blur", () => {
+            check(inputEmail)
+            checkEmail(inputEmail)
+        })
+
+        inputPhone.addEventListener("blur", () => {
+            check(inputPhone)
+            checkPhone(inputPhone)
+        })
+
+        btn.addEventListener("click", () => {
+            inputName.value = ""
+            inputEmail.value = ""
+            inputPhone.value = ""
+            inputMess.value = ""
+        })
     })
 
-    formModal.addEventListener("submit", (e) => {
-        e.preventDefault()
+    formModal2.forEach(item => {
+        let btn = item.querySelector(".form-btn")
+        let inputName = item.querySelector(".form-name")
+        let inputEmail = item.querySelector(".form-email")
+        let inputPhone = item.querySelector(".form-phone")
 
-        let itemName = formModal.querySelector(".form-name")
-        let itemEmail = formModal.querySelector(".form-email")
-        let itemPhome = formModal.querySelector(".form-phone")
+        inputName.addEventListener("blur", () => {
+            check(inputName)
+            checkName(inputName)
+        })
 
-        validForm(itemName, itemEmail, itemPhome, mess)
-        itemName.value = ""
-        itemEmail.value = ""
-        itemPhome.value = ""
+        inputEmail.addEventListener("blur", () => {
+            check(inputEmail)
+            checkEmail(inputEmail)
+        })
+
+        inputPhone.addEventListener("blur", () => {
+            check(inputPhone)
+            checkPhone(inputPhone)
+        })
+        
+        btn.addEventListener("click", () => {
+            inputName.value = ""
+            inputEmail.value = ""
+            inputPhone.value = ""
+        })
     })
-    formReuest.addEventListener("submit", (e) => {
-        e.preventDefault()
 
-        let itemName = formReuest.querySelector(".form-name")
-        let itemEmail = formReuest.querySelector(".form-email")
-        let itemPhome = formReuest.querySelector(".form-phone")
-        let itemMess = formReuest.querySelector(".mess")
-
-        validForm(itemName, itemEmail, itemPhome, itemMess)
-        itemName.value = ""
-        itemEmail.value = ""
-        itemPhome.value = ""
-        itemMess.value = ""
+    calcItem.forEach(item => {
+        item.addEventListener("blur", () => {
+            if (!validNumber.test(item.value)) {
+                console.log("число");
+            } else {
+                item.value = item.value.replace(validNumber, "")
+            }
+        })
     })
-
 }
 
 export default validate
